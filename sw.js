e6c3adcf7360db8534f56acfe3873de2c3f62f82
hungnt166-boop <hungnt166@gmail.com>
2025-11-24 22:05:56 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = 'dubai-trip-v12';
+const CACHE_NAME = 'dubai-trip-v11';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -65,4 +65,3 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
-
